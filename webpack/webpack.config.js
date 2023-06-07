97eb5ee2782720660124e4c2b3b9718827f89f13
Kidd254 <lorenkioko@gmail.com>
2023-06-07 20:39:46 +0300

    fix linter errors

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,24 +1,54 @@
- const path = require('path');
+const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/output.js',
-   },
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/output.js',
+  },
+  devtool: 'inline-source-map',
+
+  devServer: {
+
+    static: './dist',
+
+  },
 
   plugins: [
 
     new HtmlWebpackPlugin({
 
-        template: './src/index.html',
+      template: './src/index.html',
+      title: 'Development',
 
     }),
 
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+
+};
