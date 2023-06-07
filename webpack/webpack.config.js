d9a1a9ef43ad492ed0b50cd28623853af19b5da4
Kidd254 <lorenkioko@gmail.com>
2023-06-07 19:55:41 +0300

    setup webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,24 @@
+ const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+     print: './src/output.js',
+   },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+        template: './src/index.html',
+
+    }),
+
+  ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+ };
