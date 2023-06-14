e0a647702d7cb87fcfb394ce3cabdfb73f2670b5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-14 15:55:42 -0600

    fix test for adding a task

--- src/modules/removeAdd.test.js
+++ src/modules/removeAdd.test.js
@@ -9,11 +9,24 @@ const newElement = document.createElement('li');
 const myTodo = new Todo('Example Task', false, 1);
 document.body.innerHTML = '<div><ul id="taskList"></ul></div>';
 
+
+const updatedTodos = myTodo.addTodo();
 describe('add task', () => {
-  test('add task to storage', () => {
-    const updatedTodos = myTodo.addTodo();
-    expect(updatedTodos).toContainEqual([myTodo]);
-  });
+  describe('add Task', () => {
+
+      test('add task.completed to storage', () => {
+          expect(updatedTodos[0].completed).toBe([myTodo][0].completed);
+      });
+
+      test('add task.description to storage', () => {
+          expect(updatedTodos[0].description).toBe([myTodo][0].description);
+      });
+      test('add task.index to storage', () => {
+          expect(updatedTodos[0].index).toBe([myTodo][0].index);
+      });
+
+  })
+
   appendToDOM(newElement);
   test('append newElement to Dom', () => {
     const taskList = document.querySelectorAll('#taskList li');
