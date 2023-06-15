479a192552b3e883733c7a4fe42c9a3d6f5f44c0
Kidd254 <lorenkioko@gmail.com>
2023-06-15 23:39:44 +0300

    add clear completed tasks' test

--- src/modules/removeAdd.test.js
+++ src/modules/removeAdd.test.js
@@ -59,3 +59,52 @@ describe('Edit task description', () => {
     expect(description).toBe('updatedDescription');
   });
 });
+
+// test for completion update using check box
+describe('edit task completion using  checkbox', () => {
+  it('edits a task checkbox in local storage', () => {
+    // Arrange
+    const completedTask = { ...myTodo };
+    completedTask.completed = true;
+    const incompleteTask = { ...myTodo };
+    incompleteTask.completed = false;
+
+    // Act
+    const myTask = new Todo(incompleteTask);
+    const modifiedArray = myTask.updateTodo(completedTask);
+    const completedTaskArray = modifiedArray.filter(
+      (e) => e.index === completedTask.index,
+    );
+    const { completed } = completedTaskArray[0];
+
+    // Assert
+    expect(completed).toBeTruthy();
+    expect(completed).not.toBeFalsy();
+  });
+});
+
+/*describe('clear all completed', () => {
+  // Arrange
+  const task1 = new Todo('test1', true, 1);
+  const task2 = new Todo('test2', true, 2);
+  const task3 = new Todo('test3', true, 3);
+  const task4 = new Todo('test3', true, 4);
+  const modifiedArray = [task1, task2, task3, task4];
+  test('clear All Four(4) completed tasks in local storage', () => {
+    // Act
+    const newUpdatedArr = Todo.clearAllCompletedTask(modifiedArray);
+    // Assert
+    expect(newUpdatedArr).toHaveLength(0);
+  });
+
+  test('clear 2 completed tasks in local storage from 4 tasks', () => {
+    // Arrange
+    task4.completed = false;
+    task3.completed = false;
+    const modifiedArray = [task1, task2, task3, task4];
+    // Act
+    const newUpdatedArr = Todo.clearAllCompletedTask(modifiedArray);
+    // Assert
+    expect(newUpdatedArr).toHaveLength(2);
+  });
+});*/
